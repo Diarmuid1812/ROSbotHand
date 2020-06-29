9d2aca8c2f0c06ea0a2ab306662d58deadc3f42c
Ewa Ziętek <blackhood@tlen.pl>
2020-06-29 12:14:18 +0200

    standaryzacja stringa

--- src/ADC_Node.cpp
+++ src/ADC_Node.cpp
@@ -23,7 +23,7 @@ const uint8_t i2caddress = 0x04;
 const uint8_t startVoltRegAddr = 0X20;
 const int nchannels = 8;
 
-const size_t samples = 2000;
+const size_t samples = 1500;
 double threshhold;
 
 double convert(uint16_t val);
@@ -62,6 +62,7 @@ int main(int argc, char** argv)
     std::vector<uint16_t> measurements(nchannels);
     bool readingOk = true;
     std::vector<std::vector<uint16_t>> data(samples);
+    std::vector<std::vector<uint16_t>> temp(samples);
     std::vector<double> converteed_val(nchannels);
     clock_t t, start, stop;
 
@@ -83,14 +84,13 @@ int main(int argc, char** argv)
         {
             converteed_val[i] = convert(measurements[i]);
             //std::cout << converteed_val[i] << " | ";
-            ss << std::to_string(converteed_val[i]);
         }
         //std::cout << std::endl;
         
         if (moveDetected(converteed_val, threshhold))
         {
             data[0] = measurements;
-            for (int i = 1; i < 1500; ++i)
+            for (int i = 1; i < samples; ++i)
             {
                 clocks = clock() - t;
 
@@ -103,14 +103,36 @@ int main(int argc, char** argv)
                     std::cout << "ERROR while reading" << std::endl;
                     break;
                 }
-                data[i] = measurements;
+                data[i] = measurements;              
+                
             }
             std::cout << "===========STOP MEASUREMENT===================" << std::endl;
             //msg.samples = get_zero_crossing(data);
             
+            
+            /******************** konwertowanie danych ******************
+            * zapisuje calosc do wielkiego sringa
+            * tworzac macierz [8][1500] (tak uklada dane, ale w jednym wierszu zeby pozniej latwiej bylo sparsowac)
+            * poszczegolne dane oddzielone sa spacja
+            * na koncu kazdego wiersza (po 8 danych) jest ;
+            * przyklad: "1 2 3 4 5 6 7 8; 1 2 3 4 5 6 7 8; ... ; 1 2 3 4 5 6 7 8"
+            * ***********************************************************/
+            
+            for(int i = 0; i < samples; i++)	// 1500
+            {
+				for(int j = 0; j < nchannels; j++)	// 8
+				{
+					temp[i][j] = convert(data[i][j]);	//dla bezpieczenstwa zapisywane w oddzielnej tablicy
+					
+					ss << std::to_string(temp[i][j]);
+					if(j != nchannels - 1) ss << ' '; //po ostatniej probce w wierszu nie dodaje spacji
+				}
+				
+				if(i != samples - 1) ss << ';'; //po ostatnim wierszu nie dodaje srednika
+			}
             msg.data = ss.str();
             adc_publisher.publish(msg);
-            ROS_INFO("%s", msg.data.c_str());
+            //ROS_INFO("%s", msg.data.c_str()); // wypisuje wiadomosc w terminalu (bedzie strasznie dluga wiec moze lepiej nie)
             ros::spinOnce(); //do odbierania pakietow - niepotrzebne ale dobra praktyka
 			loop_rate.sleep();
         }
